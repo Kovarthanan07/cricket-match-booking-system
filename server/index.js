3a7590dee4460ee48d75ea51eb25095c7f7d3350
Kovarthanan07 <kovarthanan07@gmail.com>
2021-11-21 12:57:20 +0530

    node initiated

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const cors = require('cors');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bodyparser = require('body-parser');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const CronJob = require('cron').CronJob;
+const app = express();
+
+// view engine setup
+app.engine(
+  'handlebars',
+  exphbs({ extname: 'hbs', defaultLayout: false, layoutsDir: 'views/ ' })
+);
+app.set('view engine', 'handlebars');
+
+// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('./uploads', express.static('uploads'));
+
+//static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const port = 5002;
+
+require('dotenv').config();
+require('./db/mongoose');
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Cricket booking system backend at http://localhost:${port}`);
+});
